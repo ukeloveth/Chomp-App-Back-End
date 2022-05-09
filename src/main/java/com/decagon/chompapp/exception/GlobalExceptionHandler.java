b8f744403b8884c2ad2d345481bb01e1b05e3360
Deenn <66239065+Deennn@users.noreply.github.com>
2022-05-09 10:40:55 +0100

    Exceptions (#20)
    * added some exceptions
    
    * create global exceptions

---
+++ src/main/java/com/decagon/chompapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.decagon.chompapp.exception;
+
+import com.decagon.chompapp.dto.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleProductNotFoundException(ProductNotFoundException exception,
+                                                                        WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails( new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                                        WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails( new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+
+
+    }
+}
